02eca97038001d4848410d4184832fe94a2b8eb0
Damian <kolasa.damian@outlook.com>
2023-04-27 21:52:48 +0200

    sky-garage

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const btnMenu = document.querySelector('.nav__menu-btn');
 const mobileMenu = document.querySelector('.nav__menu-mobile-links');
 const footerYear = document.querySelector('.footer__year');
+const cookieBox = document.querySelector('.cookie-box');
+const cookieBtn = document.querySelector('.cookie-box__btn');
 
 const handleMenu = () => {
   mobileMenu.classList.toggle('menu-active');
@@ -14,10 +16,24 @@ const handleMenu = () => {
   });
 };
 
+const showCookie = () => {
+  const cookieEaten = localStorage.getItem('cookie');
+  if (cookieEaten) {
+    cookieBox.classList.add('cookie-hide');
+  }
+};
+
+const handleCookieBox = () => {
+  localStorage.setItem('cookie', 'true');
+  cookieBox.classList.add('cookie-hide');
+};
+
 const handleCurrentYear = () => {
   const year = new Date().getFullYear();
   footerYear.innerText = year;
 };
-handleCurrentYear();
 
+handleCurrentYear();
+showCookie();
 btnMenu.addEventListener('click', handleMenu);
+cookieBtn.addEventListener('click', handleCookieBox);
